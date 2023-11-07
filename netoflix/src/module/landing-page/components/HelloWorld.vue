1658b7f07a90eb9864e466f925df2411f8030a51
Neto6391 <neto6391@gmail.com>
2023-11-07 14:27:03 -0300

    feat(struct folders for clean arch)

---
+++ netoflix/src/module/landing-page/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+const { msg } = defineProps({
+    msg: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div>
+        {{ msg }}
+    </div>
+</template>
+
+<style scoped>
+
+</style>
